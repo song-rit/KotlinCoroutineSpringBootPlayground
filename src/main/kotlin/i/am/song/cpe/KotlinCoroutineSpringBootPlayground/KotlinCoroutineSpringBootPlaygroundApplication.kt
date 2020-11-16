30dec7bc757ed65a021abb13911716773c155108
songrit.m <song.cpe@gmail.com>
2020-11-16 16:15:15 +0700

    initial project

---
+++ src/main/kotlin/i/am/song/cpe/KotlinCoroutineSpringBootPlayground/KotlinCoroutineSpringBootPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package i.am.song.cpe.KotlinCoroutineSpringBootPlayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutineSpringBootPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCoroutineSpringBootPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/i/am/song/cpe/KotlinCoroutineSpringBootPlayground/KotlinCoroutineSpringBootPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package i.am.song.cpe.KotlinCoroutineSpringBootPlayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutineSpringBootPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
