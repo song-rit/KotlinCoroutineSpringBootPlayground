6e34f765714822bc51d24098b4717937ca04ce74
songrit.m <song.cpe@gmail.com>
2020-11-19 21:15:51 +0700

    change to private method

--- src/main/kotlin/i/am/song/cpe/playground/ComposingSuspendingFunctions.kt
+++ src/main/kotlin/i/am/song/cpe/playground/ComposingSuspendingFunctions.kt
@@ -19,12 +19,12 @@ class ComposingSuspendingFunctions {
     }
 
     // The result type of somethingUsefulOneAsync is Deferred<Int>
-    fun somethingUsefulOneAsync() = GlobalScope.async {
+    private fun somethingUsefulOneAsync() = GlobalScope.async {
         doSomethingUsefulOne()
     }
 
     // The result type of somethingUsefulTwoAsync is Deferred<Int>
-    fun somethingUsefulTwoAsync() = GlobalScope.async {
+    private fun somethingUsefulTwoAsync() = GlobalScope.async {
         doSomethingUsefulTwo()
     }
 
