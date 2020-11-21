b28bafe90930eaced73b0a5638bc7d18e8b5062b
songrit.m <song.cpe@gmail.com>
2020-11-21 23:01:50 +0700

    poc: launch inherits from coroutine scope

--- src/main/kotlin/i/am/song/cpe/playground/CoroutineContextAndDispatchers.kt
+++ src/main/kotlin/i/am/song/cpe/playground/CoroutineContextAndDispatchers.kt
@@ -1,14 +1,18 @@
 package i.am.song.cpe.playground
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class CoroutineContextAndDispatchers {
 
     fun dispatcherAndThread() {
         runBlocking {
+
+            GlobalScope.launch {
+                launch { // context of the parent, main runBlocking coroutine
+                    println("main GlobalScope      : I'm working in thread ${Thread.currentThread().name}")
+                }
+            }
+
             launch { // context of the parent, main runBlocking coroutine
                 println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
             }
@@ -23,7 +27,12 @@ class CoroutineContextAndDispatchers {
 
             launch(newSingleThreadContext("MyOwnThread")) { // will get its own new thread
                 println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+
+                launch { // context of the parent, main runBlocking coroutine
+                    println("main newSingleThreadContext      : I'm working in thread ${Thread.currentThread().name}")
+                }
             }
+
         }
     }
 }
